d798503c284c02e8982eef13115ab93e01d5bafa
Saswat <mohantysaswat154@gmail.com>
2026-03-16 09:53:39 +0530

    writing first test

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>
+        We are glad to have you here. Feel free to explore and enjoy your stay.
+      </p>
+    </div>
+  );
+};
+
+export default Greeting;
